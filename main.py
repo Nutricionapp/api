044c89494a5bb350d5d2142e8776952ebb255eeb
FelipeH22 <juan22herrera01@gmail.com>
2023-05-29 23:37:43 -0500

    Cambios

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ app = FastAPI()
 
 origins = [
     "http://localhost:3000",
-    "https://nutricion-app-frontend.vercel.app/"
+    "https://190.9.202.46:0",
+    "http://190.9.202.46:0"
 ]
 
 app.add_middleware(
