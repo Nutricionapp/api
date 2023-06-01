9a32956249deb4110b221c4a56a7a6c2e8a97852
FelipeH22 <juan22herrera01@gmail.com>
2023-05-31 23:07:02 -0500

    Algunos cambios

--- NutricionApp/database.py
+++ NutricionApp/database.py
@@ -1,9 +1,10 @@
+import psycopg2
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHAMY_DATABASE_URL = 'sqlite:///./NutricionApp.db'
-engine = create_engine(SQLALCHAMY_DATABASE_URL,connect_args={"check_same_thread": False})
+SQLALCHAMY_DATABASE_URL = 'postgresql://nutricionapp_rzuk_user:kWPcQqdeZSu4osYCrCJIf6AIHShqflqb@dpg-chs0o3e4dadfn67en5g0-a/nutricionapp_rzuk'
+engine = create_engine(SQLALCHAMY_DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False,)
 Base = declarative_base()
 def get_db():
