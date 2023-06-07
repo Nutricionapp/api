212410d532c1f1321237d7bf8c59c2b0a8637e89
scasasr <CORREO@example.com>
2023-06-07 13:13:58 -0500

    MercadoPago #2

---
+++ NutricionApp/repository/mercadopago.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+# SDK de Mercado Pago
+import mercadopago
+# Agrega credenciales
+sdk = mercadopago.SDK("PROD_ACCESS_TOKEN")
+
+class dataMercadoPago (BaseModel):#"no es necesario guardar modelo, solo necesitamos generar el id para usarlo en el frontend"
+    title:str
+    quantity:int 
+    price:float
+    timeStampExpiration:str
+
+
+def getPreferenceID(Mercadopago:dataMercadoPago):#puse get como nombre pero tiene que usarse un metodo post que reciba la data que esta arriba y que responda el id de abajo
+    # Crea un elemento en la preferencia
+    preference_data = {
+    # el "purpose": "wallet_purchase", solo permite pagos registrados
+    # para permitir pagos de invitados, puede omitir esta propiedad
+        "purpose": "wallet_purchase",
+        "items": [
+            {
+                "title": Mercadopago.title,
+                "quantity": Mercadopago.quantity,
+                "unit_price": Mercadopago.price
+            }
+        ],
+        "date_of_expiration": Mercadopago.timeStampExpiration,
+        "back_urls": {
+            "success": "https://nutricion-app-frontend.vercel.app/",
+            "failure": "https://nutricion-app-frontend.vercel.app/",
+            "pending": "https://www.tu-sitio/pendings"
+        },
+        "auto_return": "approved"
+    }
+    preference_response = sdk.preference().create(preference_data)
+    preference = preference_response["response"]
+
+    return {"preferenceId":preference}
+
